2d17ce03768d07ad3238eb66914bba104fcf3ca1
Sharath H K <158291023+Sharathhk122@users.noreply.github.com>
2025-08-06 12:58:34 +0530

    Update auth.service.js

--- frontend/src/services/auth.service.js
+++ frontend/src/services/auth.service.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api/auth/';
+const API_URL = 'https://securelogin-tyer.onrender.com/api/auth';
 
 class AuthService {
   login(username, password) {
-    return axios.post(API_URL + 'signin', {
+    return axios.post(API_URL + '/signin', {
       username,
       password
     }, {
@@ -41,7 +41,7 @@ class AuthService {
   }
 
   register(username, password) {
-    return axios.post(API_URL + 'signup', {
+    return axios.post(API_URL + '/signup', {
       username,
       password
     }, {
