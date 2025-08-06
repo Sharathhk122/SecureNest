d5e3f6b2133d8356c1d7eb91c7a741703e095514
Sharath H K <158291023+Sharathhk122@users.noreply.github.com>
2025-08-06 14:38:28 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   server: {
-    host: true, // needed for the Docker Container port mapping to work
+  server: {
+    host: true,
     strictPort: true,
-    port: 5173, // you can replace this port with any port
+    port: 5173,
+    allowedHosts: ['securenest-sharathhk.onrender.com'], // ✅ Add this line
   }
 })
